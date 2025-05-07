6a62ae2a62294328bc0c35c04857f69a0fd4d81e
Ajeet Atpl <ajeet.yadav@atpl.live>
2025-05-07 12:14:56 +0530

    added docker compose

---
+++ first-image/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello, World!"
+echo "This is a simple script."
+echo "It prints messages to the console."
+echo "This is the end of the script."
+echo "Goodbye!"
+
+# This is a comment
+echo "This line is not executed."
+echo "This is another line."
+echo "This is a test."
+echo "This is a test of the script."
+echo "This is a test of the script."
